e37424644c542355d68a3149a0bb36d5a079144b
rian <riandiuniska@gmail.com>
2020-04-22 15:31:18 +0800

    Array - Cara Lain Mendefinisikan Array

--- src/array/Main.java
+++ src/array/Main.java
@@ -2,10 +2,17 @@ package array;
 
 public class Main {
     public static void main(String[] args) {
-       int nilaiArr[] = {10,20,30,80,50,70,60,90,40};
+        String mataKuliah[];
+        mataKuliah = new String[5];
 
-        System.out.println(nilaiArr.length); //jumlah/panjang array
-        System.out.println(nilaiArr[0]); //index pertama
-        System.out.println(nilaiArr[8]); // index terakhir
+        mataKuliah[0] = "PBO 1";
+        mataKuliah[1] = "Visual 2";
+        mataKuliah[2] = "RPL";
+        mataKuliah[3] = "ORKOM";
+        mataKuliah[4] = "WEB 2";
+
+        System.out.println(mataKuliah.length); //jumlah/panjang array
+        System.out.println(mataKuliah[0]); //index pertama
+        System.out.println(mataKuliah[4]); // index terakhir
     }
 }
