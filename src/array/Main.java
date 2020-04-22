c873064d5f9fe6f874bc5d4419bd6d12d77a9c8c
rian <riandiuniska@gmail.com>
2020-04-22 15:27:07 +0800

    Array - Mendefinisikan Array

---
+++ src/array/Main.java
@@ -0,0 +1,11 @@
+package array;
+
+public class Main {
+    public static void main(String[] args) {
+       int nilaiArr[] = {10,20,30,80,50,70,60,90,40};
+
+        System.out.println(nilaiArr.length); //jumlah/panjang array
+        System.out.println(nilaiArr[0]); //index pertama
+        System.out.println(nilaiArr[8]); // index terakhir
+    }
+}
