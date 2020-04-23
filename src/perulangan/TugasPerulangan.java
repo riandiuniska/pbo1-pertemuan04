422234480d2391ae5f8e99450ab25de209199607
rian <riandiuniska@gmail.com>
2020-04-23 17:20:35 +0800

    Perulangan For - Perkalian Sederhana

---
+++ src/perulangan/TugasPerulangan.java
@@ -0,0 +1,17 @@
+package perulangan;
+
+import java.util.Scanner;
+
+public class TugasPerulangan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //Untuk mengimput nilai/data dari user
+
+        System.out.println ("==========|PERKALIAN|==========");
+        System.out.print ("Input angka untuk dikali = ");
+        int angka = input.nextInt();
+
+        for (int i = 1; i <= 10; ++i)
+            System.out.println (i+". Hasil dari Perkalian "+i+" x "+angka+" = "+(i*angka));
+
+    }
+}
